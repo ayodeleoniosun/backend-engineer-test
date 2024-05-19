9fe60d04bbcbfd5a31965cb11e284ebab2fa9961
Ayodele Oniosun <ayodeleoniosun63@gmail.com>
2024-05-19 05:22:12 +0100

    test: unit test for index, show and view my products

--- tests/unit/product.service.spec.ts
+++ tests/unit/product.service.spec.ts
@@ -3,7 +3,7 @@ import ProductModel from "../../src/models/product.model";
 import {testData} from '../seed/user.test';
 import {login, register} from "../../src/services/auth.service";
 import UserModel from "../../src/models/user.model";
-import {create, update} from "../../src/services/product.service";
+import {create, destroy, index, myProducts, show, update} from "../../src/services/product.service";
 import {createProductData, productData} from "../seed/product.test";
 import {ErrorMessages} from "../../src/utils/enums/error.messages";
 import {ObjectId} from 'mongodb';
@@ -24,6 +24,43 @@ describe('Product unit tests', () => {
         user = await register(testData);
         loggedUser = await login(user.email, testData.password);
         await create(productData, loggedUser!.user._id);
+    });
+
+    describe('All Products', () => {
+        it('it should display all products', async () => {
+            await create(createProductData, loggedUser!.user._id);
+
+            let payload = JSON.parse(JSON.stringify(createProductData));
+            payload.name = 'second product name';
+
+            await create(payload, loggedUser!.user._id);
+
+            const response = await index();
+
+            expect(response.length).toBe(3);
+        });
+    });
+
+    describe('My Products', () => {
+        it('it should display my products', async () => {
+            await create(createProductData, loggedUser!.user._id);
+
+            let payload = JSON.parse(JSON.stringify(createProductData));
+            payload.name = 'second product name';
+
+            await create(payload, loggedUser!.user._id);
+
+            //create another user and his products
+            let anotherUserPayload = JSON.parse(JSON.stringify(testData));
+            anotherUserPayload.email = 'anotheruser@example.com';
+
+            const anotherUser = await register(anotherUserPayload);
+            await create(createProductData, anotherUser._id);
+
+            const response = await myProducts(anotherUser._id);
+
+            expect(response.length).toBe(1);
+        });
     });
 
     describe('Create Product', () => {
@@ -81,6 +118,50 @@ describe('Product unit tests', () => {
             expect(response!.name).toBe(payload.name);
             expect(response!.description).toBe(payload.description);
             expect(response!.price).toBe(payload.price);
+        });
+    });
+
+    describe('Show Product', () => {
+        it('it should throw an error if a product does not exist', async () => {
+            try {
+                const invalidId = new ObjectId().toString();
+
+                await show(invalidId);
+            } catch (err: any) {
+                expect(err.message).toBe(ErrorMessages.PRODUCT_NOT_FOUND);
+            }
+        });
+
+        it('it should show product details', async () => {
+            const createProduct = await create(createProductData, loggedUser!.user._id);
+            const response = await show(createProduct._id);
+
+            expect(response.length).toBe(1);
+            expect(response[0]!.name).toBe(createProduct.name);
+            expect(response[0]!.description).toBe(createProduct.description);
+            expect(response[0]!.price).toBe(createProduct.price);
+        });
+    });
+
+    describe('Delete Product', () => {
+        it('it should throw an error if a product does not exist', async () => {
+            try {
+                const invalidId = new ObjectId().toString();
+
+                await destroy(invalidId, loggedUser!.user._id);
+            } catch (err: any) {
+                expect(err.message).toBe(ErrorMessages.PRODUCT_NOT_FOUND);
+            }
+        });
+
+        it('it should delete product', async () => {
+            const createProduct = await create(createProductData, loggedUser!.user._id);
+            const response = await destroy(createProduct._id, loggedUser!.user._id);
+
+            expect(response!._id.toString()).toBe(createProduct._id.toString());
+            expect(response!.name).toBe(createProduct.name);
+            expect(response!.description).toBe(createProduct.description);
+            expect(response!.price).toBe(createProduct.price);
         });
     });
 });
